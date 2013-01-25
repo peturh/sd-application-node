20bf810d5d4830662ab3db2598f61014f79c6e3c
davdah <davdah@SEMALL0047.SEC.INTRA>
2013-01-25 15:30:17 +0100

    databse

--- sd-database-connector/src/dbtest.java
+++ sd-database-connector/src/dbtest.java
@@ -11,10 +11,21 @@ public class dbtest {
 	public static void main(String[] args) {
 		Database db = new Database();
 		db.openConnection("dm","654321");
-		db.doStatement("select * from shard03.event_log where installation_id = 5768");
+		
 		//db.doStatement("select * from dm_red.installation where installation_id = 5768");
 		
-		
+		long t= System.currentTimeMillis();
+		long end = t+60000;
+		while(System.currentTimeMillis() < end) {
+		  db.doStatement("select * from shard03.event_log where installation_id = 5768");
+		  
+		  try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
 		
 		db.closeConnection();
 		
