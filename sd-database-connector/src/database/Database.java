8a481ed9819ae3510c890d9735bed8d92bb30fa0
davdah <davdah@SEMALL0047.SEC.INTRA>
2013-01-25 14:23:13 +0100

    database update

--- sd-database-connector/src/database/Database.java
+++ sd-database-connector/src/database/Database.java
@@ -1,6 +1,5 @@
 package database;
 
-import java.util.ArrayList;
 import java.sql.*;
 
 /**
@@ -78,32 +77,27 @@ public class Database {
 		return conn != null;
 	}
 
-	public void doStatement(String query){
+	public void doStatement(String query) {
 		ResultSet rs = null;
 		try {
-			rs= stmt.executeQuery(query);
+			rs = stmt.executeQuery(query);
+
 			
-			  
-			  
-			  
-			  while (rs.next()) {
-				  System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + " " + rs.getString(3) + " " 
-						  + " " + rs.getString(4) + " " + " " + rs.getString(5) + " " + " " + rs.getString(6) + " " 
-						  + " " + rs.getString(7) + " " + " " + rs.getString(8) + " " + " " + rs.getString(9));
-				  
-				  }
-			  stmt.close();
-			  rs.close();
-			  conn.close();
+			
+			while (rs.next()) {
+
+				rs.first();
+				
+				System.out.println("Hej");
+			}
+
+			stmt.close();
+			rs.close();
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
-	
 
-		
-	
-	
-
-}
+	}
 }
