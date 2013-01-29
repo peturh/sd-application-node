d258ab8a3e50d9d10074f4c5dde20fa36c9d0b82
pethja <petur.hjartarson@securitas-direct.com>
2013-01-29 10:14:53 +0100

    new sd-web-app

---
+++ sd-web-app/src/sd/web/app/server/Database.java
@@ -0,0 +1,106 @@
+package sd.web.app.server;
+
+
+import java.sql.*;
+
+/**
+ * Database is a class that specifies the interface to the movie database. Uses
+ * JDBC and the MySQL Connector/J driver.
+ */
+public class Database {
+	/**
+	 * The database connection.
+	 */
+	private Connection conn;
+	
+	private int latest;
+
+	/**
+	 * An SQL statement object.
+	 */
+	private Statement stmt;
+
+	/**
+	 * Create the database interface object. Connection to the database is
+	 * performed later.
+	 */
+	public Database() {
+		conn = null;
+		latest=0;
+	}
+
+	/**
+	 * Open a connection to the database, using the specified user name and
+	 * password.
+	 * 
+	 * @param userName
+	 *            The user name.
+	 * @param password
+	 *            The user's password.
+	 * @return true if the connection succeeded, false if the supplied user name
+	 *         and password were not recognized. Returns false also if the JDBC
+	 *         driver isn't found.
+	 */
+	public boolean openConnection(String userName, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://10.21.5.196", userName,
+					password);
+			
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+
+
+	/**
+	 * Close the connection to the database.
+	 */
+	public void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+		}
+		conn = null;
+	}
+
+	/**
+	 * Check if the connection to the database has been established
+	 * 
+	 * @return true if the connection has been established
+	 */
+	public boolean isConnected() {
+		return conn != null;
+	}
+
+	public String doStatement(String query) {
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			rs = stmt.executeQuery(query);
+			
+			while(rs.next()){
+				sb.append(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" \n" );
+				
+				}
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return sb.toString();
+
+	}
+	
+}
---
+++ sd-web-app/src/sd/web/app/server/GreetingServiceImpl.java
@@ -0,0 +1,43 @@
+package sd.web.app.server;
+
+import sd.web.app.client.GreetingService;
+import sd.web.app.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+	public String greetServer(String input) throws IllegalArgumentException {
+		// Verify that the input is valid. 
+		
+		Database db = new Database();
+		db.openConnection("root","1234");
+		String result = db.doStatement("select * from test.dev;");
+
+		// Escape data from the client to avoid cross-site script vulnerabilities.
+		db.closeConnection();
+
+		return "hej"; //"Hello, " + input + "!<br><br>I am running " + serverInfo
+				//+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+
+	/**
+	 * Escape an html string. Escaping data received from the client helps to
+	 * prevent cross-site script vulnerabilities.
+	 * 
+	 * @param html the html string to escape
+	 * @return the escaped string
+	 */
+	private String escapeHtml(String html) {
+		if (html == null) {
+			return null;
+		}
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;");
+	}
+}
